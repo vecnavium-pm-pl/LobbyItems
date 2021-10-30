6fd19ca9fd581dce3c6e32a5aa383e95be1e5b71
Vecnavium <vecnavium@gmail.com>
2021-10-30 10:16:34 -0700

    Updated to API 4.0.0

--- src/Vecnavium/LobbyItems/Listeners.php
+++ src/Vecnavium/LobbyItems/Listeners.php
@@ -25,11 +25,10 @@ class Listeners implements Listener {
 
     public function onInteract(PlayerInteractEvent $event) {
         $itemid = $event->getItem()->getId();
-        $itemmeta = $event->getItem()->getDamage();
         $itemname = $event->getItem()->getName();
         foreach (Main::getInstance()->lobbyitemconfigurations()->get("lobby-items") as $item) {
             $get = explode("-", $item);
-            if ($itemid == $get[0] && $itemmeta == $get[1] && $itemname === "$get[4]") Main::getInstance()->getServer()->dispatchCommand($event->getPlayer(), "$get[5]");
+            if ($itemid == $get[0] && $itemname === "$get[4]") Main::getInstance()->getServer()->dispatchCommand($event->getPlayer(), "$get[5]");
         }
     }
 
--- src/Vecnavium/LobbyItems/Main.php
+++ src/Vecnavium/LobbyItems/Main.php
@@ -8,12 +8,11 @@
 
 namespace Vecnavium\LobbyItems;
 
-use pocketmine\Player;
-use pocketmine\item\Item;
+use pocketmine\player\Player;
+use pocketmine\item\ItemFactory;
 use pocketmine\utils\Config;
 use pocketmine\plugin\PluginBase;
 use Vecnavium\EventListener;
-use Vecnavium\LobbyItems\CheckUpdateTask;
 
 class Main extends PluginBase {
 
@@ -29,21 +28,15 @@ class Main extends PluginBase {
         return new Config($this->getDataFolder() . "config.yml", Config::YAML);
     }
 
-    public function onEnable()
+    public function onEnable(): void
     {
         self::$instance = $this;
         $this->getServer()->getPluginManager()->registerEvents(new Listeners(), $this);
         $this->saveResource("config.yml");
-        $this->checkUpdate();
-    }
-
-    public function checkUpdate(bool $isRetry = false): void {
-
-        $this->getServer()->getAsyncPool()->submitTask(new CheckUpdateTask($this, $isRetry));
     }
 
     public function giveItems(Player $player, $get) {
-        $player->getInventory()->setItem($get[3], Item::get($get[0], $get[1], $get[2])->setCustomName($get[4]));
+        $player->getInventory()->setItem($get[3], ItemFactory::getInstance()->get($get[0], $get[1], $get[2])->setCustomName($get[4]));
 
     }
 
