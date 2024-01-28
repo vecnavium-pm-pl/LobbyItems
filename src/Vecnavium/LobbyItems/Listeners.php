8496e9da98ac1f20f5a7f4f054d1f3928de0666a
nicholass003 <veganicholas123@gmail.com>
2024-01-29 02:38:41 +0700

    feat: 🔖 update to API 5.0.0

--- src/Vecnavium/LobbyItems/Listeners.php
+++ src/Vecnavium/LobbyItems/Listeners.php
@@ -13,31 +13,34 @@ use pocketmine\event\inventory\InventoryTransactionEvent;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerDropItemEvent;
+use pocketmine\item\Item;
 
 class Listeners implements Listener {
 
-
-    public function onJoin(PlayerJoinEvent $event) {
-        foreach (Main::getInstance()->lobbyitemconfigurations()->get("lobby-items") as $item) {
+    public function onJoin(PlayerJoinEvent $event) : void{
+        foreach (Main::getInstance()->getConfig()->get("lobby-items") as $item) {
             Main::getInstance()->giveItems($event->getPlayer(), explode("-", $item));
         }
     }
 
-    public function onInteract(PlayerInteractEvent $event) {
-        $itemid = $event->getItem()->getId();
-        $itemname = $event->getItem()->getName();
-        foreach (Main::getInstance()->lobbyitemconfigurations()->get("lobby-items") as $item) {
-            $get = explode("-", $item);
-            if ($itemid == $get[0] && $itemname === "$get[4]") Main::getInstance()->getServer()->dispatchCommand($event->getPlayer(), "$get[5]");
+    public function onInteract(PlayerInteractEvent $event) : void{
+        $player = $event->getPlayer();
+        $itemA = $event->getItem();
+        if($itemA->getNamedTag()->getTag(Main::TAG_LOBBY_ITEM) === null) return;
+        foreach(Main::getInstance()->getLobbyItems() as $itemB){
+            /** @var Item $itemB */
+            if($itemA->equals($itemB, false, true)){
+                Main::getInstance()->getServer()->dispatchCommand($player, $itemB->getNamedTag()->getTag(Main::TAG_ITEM_COMMAND)->getValue());
+                break;
+            }
         }
     }
 
-    public function onDrop(PlayerDropItemEvent $event) {
-        if(Main::getInstance()->lobbyitemconfigurations()->getNested("events.drop-inventory-items") == false) $event->cancel();
+    public function onDrop(PlayerDropItemEvent $event) : void{
+        if(Main::getInstance()->getConfig()->getNested("events.drop-inventory-items") == false) $event->cancel();
     }
 
-    public function onInventoryMove(InventoryTransactionEvent $event) {
-        if(Main::getInstance()->lobbyitemconfigurations()->getNested("events.move-inventory-items") == false) $event->cancel();
+    public function onInventoryMove(InventoryTransactionEvent $event) : void{
+        if(Main::getInstance()->getConfig()->getNested("events.move-inventory-items") == false) $event->cancel();
     }
-
 }
